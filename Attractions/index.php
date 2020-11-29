dab0c813168b73df3327fa0c5e8f3dacc5680f90
Lebonid <ufoleo26@gmail.com>
2020-11-29 20:37:29 +0300

    Добавлена авторизация и регистрация

---
+++ Attractions/index.php
@@ -0,0 +1,18 @@
+<html>
+	<head>
+	</head>
+	<body>
+	<form action="" method="POST">
+		<br>Login: <input type="text" name="t1"></br>
+		<br>Password <input type="password" name="t2"></br>
+		<input type="submit" value="OK">
+	</form>
+	</body>
+</html>
+
+<?php
+	$dbconn = mysqli_connect("localhost", "root", 12345, "park", 3304);
+	
+	
+?>
+
